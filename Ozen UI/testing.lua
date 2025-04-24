316b953747a72b021679b41580ee11382950374f
rhy <106398343+rhywme@users.noreply.github.com>
2025-04-24 20:02:23 +0200

    Update testing.lua

--- Ozen UI/testing.lua
+++ Ozen UI/testing.lua
@@ -1,5 +1,9 @@
 -- It is better NOT to use this loadstring, it may contain some errors for the UI, use the source version: https://raw.githubusercontent.com/rhywme/UI-Libraries/main/Ozen%20UI/source.lua
 
+print[[It is better NOT to use this loadstring:
+          - It may contain some errors for the UI;
+	      - Use the source version: https://raw.githubusercontent.com/rhywme/UI-Libraries/main/Ozen%20UI/source.lua]]
+
 local Library = {}
 
 function Library:CreateWindow(name)
