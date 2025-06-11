a2d77df80865b9b038b7ec14fc7d524e1046b292
rhy <106398343+rhywme@users.noreply.github.com>
2025-06-11 20:59:17 +0200

    Added dropdown element and more free comments.

--- Ozen UI/example.lua
+++ Ozen UI/example.lua
@@ -1,7 +1,12 @@
+-- [[ Ozen UI ] --]
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/rhywme/UI-Libraries/main/Ozen%20UI/source.lua"))()
 local Window = Library:CreateWindow("Ozen UI")
 
--- Add multiple tabs
+-- [[ Add multiple tabs ]] --
+
+-- You can add as many tabs as you want, just make sure to use unique names for each tab.
+
 local Tab1 = Window:AddTab("Tab 1")
 local Tab2 = Window:AddTab("Tab 2")
 local Tab3 = Window:AddTab("Tab 3")
@@ -11,11 +16,15 @@ local Tab6 = Window:AddTab("Tab 6")
 local Tab5 = Window:AddTab("Tab 7")
 local Tab6 = Window:AddTab("Tab 8")
 
--- Add elements to tabs
+-- [[ Add elements to tabs ]] --
+
+-- Label
 
 Tab1:AddLabel({
     Text = "Ozen UI was made by rhy"
 })
+
+-- Toggle
 
 Tab1:AddToggle({
     Text = "Toggle",
@@ -24,12 +33,33 @@ Tab1:AddToggle({
     end
 })
 
+
+-- Dropdown
+
+Tab1:AddDropdown({
+    Text = "Multi Select",
+    Options = {"Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6", "Option 7", "Option 8", "Option 9", "Option 10"},
+    MultipleOptions = true,
+    Callback = function(selected)
+        if type(selected) == "table" then
+            print("Selected:", table.concat(selected, ", ")) -- Multiple selections
+        else
+            print("Selected:", selected) -- Single selection
+        end
+    end
+})
+
+
+-- Button
+
 Tab1:AddButton({
     Text = "Button",
     Callback = function() 
         print("Button clicked!")
     end
 })
+
+-- Textbox
 
 Tab1:AddTextbox({
     Text = "Textbox",
@@ -39,6 +69,8 @@ Tab1:AddTextbox({
 	print(text)
     end
 })
+
+-- Slider
 
 Tab1:AddSlider({
     Text = "Walk Speed",
