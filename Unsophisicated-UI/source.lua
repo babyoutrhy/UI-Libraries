0cf62e1ff7d13aa0cf2fe6fe31a7de7b116a3a3e
rhy <106398343+babyoutrhy@users.noreply.github.com>
2026-03-19 17:01:28 +0100

    Added home decal ID>

--- Unsophisicated-UI/source.lua
+++ Unsophisicated-UI/source.lua
@@ -1140,7 +1140,7 @@ function Unsophisicated:CreateWindow(title)
     end
 
     function Window:CreateHomeTab()
-        local home = self:AddTab("Home")
+        local home = self:AddTab("Home", "6026568251")
         local content = home.Content
 
         -- Profile section
